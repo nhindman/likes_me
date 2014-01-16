adeeb955fe7062e556eb0b1a5558f3fa425c6cf6
kinzin <yniznik@gmail.com>
2014-01-16 18:11:58 -0500

    gender button styling

--- lib/photoselect.rb
+++ lib/photoselect.rb
@@ -6,15 +6,15 @@ module PhotoSelect
   
     populate_likes_and_tags!(photos, graph)
  
-    photos.map do |photo|
+    return photos.map do |photo|
       all_likes = photo["likes"]   
       
       {
         id: photo["id"],
         url: photo["source"],
         num_tags: photo["tag_count"],
-        male_likes: all_likes.count{|l| l[:gender] == "male"},
-        female_likes: all_likes.count{|l| l[:gender] == "female"},
+        male_likes: all_likes.count   { |l| l[:gender] == "male" },
+        female_likes: all_likes.count { |l| l[:gender] == "female" },
         total_likes: all_likes.length,
       }
     end
@@ -28,6 +28,24 @@ module PhotoSelect
         like[:gender] = graph.get_object(like["id"])["gender"]
       end     
     end
+
+
+
+#    photos.each do |photo|
+#       photo["tag_count"] = graph.get_connection( photo["id"], "tags" ).count
+#       photo["likes"] = graph.get_connection( photo["id"] , "likes" , {:limit => 25} )
+#       photo["likes"].each do |like|
+#         like[:gender] = graph.get_object(like["id"])["gender"]
+#       end     
+#     end
+
+# education = @graph.get_object("me") { |data| data['education'] }
+
+
+
+
+
+
   end
 
   def self.get_best_photos(photos_array, sort_by)
@@ -36,10 +54,8 @@ module PhotoSelect
     max_num_tags = 4
     min_num_likes = 1
     best_photos = []
-   # binding.pry
     if photos_array
       photos_array.each do |photo|
-        # binding.pry
         if photo[:num_tags] <= max_num_tags && photo[sort_by] >= min_num_likes
           best_photos << photo
         end
--- spec/api/webmock_spec.rb
+++ spec/api/webmock_spec.rb
@@ -3,10 +3,16 @@ require 'photoselect'
 
 describe PhotoSelect do
   describe"given a valid session token for user Zina Eskina"
-  it "should get the best photos" do
+  it "should get the query facebook for photos" do
     VCR.use_cassette('ZinaEskina#photos') do
       photos = PhotoSelect::fb_connect("CAADcvmCZAfrkBAIPwV3z3jHP1m3onftL9ffw4Kz8X80oei23qeHpNY3gDfYtvmzrF7QoJyhZAXJA9D2TT5uUV3rXS0i3cy2gUZAnXWIG3rtDynzaU7fZAmDh8bUdjIaCRRUCiSayQIsZCNyZBiCs1zL2DWRPvWnO7YvLmSNt7dDuX9WAxIH3wi")
       photos.map{|p| p[:id]}.should include("10151692051396594")
     end
   end
+  # it "should get the query facebook for photos" do
+  #   VCR.use_cassette('ZinaEskina#photos') do
+  #     photos = PhotoSelect::fb_connect("CAADcvmCZAfrkBAIPwV3z3jHP1m3onftL9ffw4Kz8X80oei23qeHpNY3gDfYtvmzrF7QoJyhZAXJA9D2TT5uUV3rXS0i3cy2gUZAnXWIG3rtDynzaU7fZAmDh8bUdjIaCRRUCiSayQIsZCNyZBiCs1zL2DWRPvWnO7YvLmSNt7dDuX9WAxIH3wi")
+  #     photos.map{|p| p[:id]}.should include("10151692051396594")
+  #   end
+  # end
 end
